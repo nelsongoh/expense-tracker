a6568bca253cfe2e15ab8d4850ce928cd87a8994
nelsongoh <61051561+nelsongoh@users.noreply.github.com>
2022-10-07 16:07:20 +0800

    refactor: Removed the unused CSS file and added a display context / provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import DisplayProvider from './context/display/provider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <DisplayProvider>
+        <App />
+      </DisplayProvider>
     </BrowserRouter>
   </React.StrictMode>
 );

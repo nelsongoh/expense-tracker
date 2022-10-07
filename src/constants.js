bfcad78c36b7b069f6e0906be3e1441964a66999
nelsongoh <61051561+nelsongoh@users.noreply.github.com>
2022-10-07 19:32:40 +0800

    refactor: Manually freeze the constants object so as not to lose Intellisense functionality

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,17 @@
-import { deepFreeze } from "./utils";
-
-const Constants = deepFreeze({
-  PATHS: {
+const Constants = Object.freeze({
+  PATHS: Object.freeze({
     INDEX: "/"
-  }
+  }),
+  CONTENT: Object.freeze({
+    FORMS: Object.freeze({
+      REGISTER: Object.freeze({
+        TITLE: "Register",
+        NAME_FIELD: "Name",
+        REGISTER_BTN: "Sign-up",
+        SWITCH_TO_LOGIN: "Login"
+      })
+    })
+  })
 });
 
 export default Constants;

ac67e00133f6afc2eb10e755e9d3d03cddf5a27d
nelsongoh <61051561+nelsongoh@users.noreply.github.com>
2022-10-07 19:34:04 +0800

    refactor: Updated the LandingScene to use the RegisterForm component

--- src/scenes/Landing/index.js
+++ src/scenes/Landing/index.js
@@ -1,6 +1,12 @@
+import { useContext } from "react";
+import DisplayContext from "../../context/display/context";
+import RegisterForm from "../../components/Forms/Register";
+
 const LandingScene = () => {
+  const { isMobileDisplay } = useContext(DisplayContext);
+
   return (
-    null
+    <RegisterForm isMobileDisplay={isMobileDisplay}/>
   );
 }
 

bb7bea2e23090c08f4cbc9fc3eddc58876411d73
nelsongoh <61051561+nelsongoh@users.noreply.github.com>
2022-10-07 15:00:45 +0800

    feat: Created a utility functions file

---
+++ src/utils.js
@@ -0,0 +1,23 @@
+/**
+ * Recursively freeze nested objects / functions within the object.
+ * @param {Object} obj 
+ * @returns A frozen object with all its frozen nested objects / functions.
+ */
+export const deepFreeze = (obj : Object) => {
+  Object.freeze(obj);
+
+  if (typeof obj === 'undefined') {
+    return obj;
+  }
+
+  Object.getOwnPropertyNames(obj).forEach((prop) => {
+    if (obj[prop] !== null && 
+      (typeof obj[prop] === "object" || typeof obj[prop] === "function") &&
+      !Object.isFrozen(obj[prop])
+    ) {
+      deepFreeze(obj[prop]);
+    }
+  });
+
+  return obj;
+}

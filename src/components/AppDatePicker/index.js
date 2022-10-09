9de3b6c00872009850401902e4753bf8530bcf0f
nelsongoh <61051561+nelsongoh@users.noreply.github.com>
2022-10-09 14:00:29 +0800

    feat: Created a wrapper datepicker component

---
+++ src/components/AppDatePicker/index.js
@@ -0,0 +1,29 @@
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import TextField from '@mui/material/TextField';
+import dayjs from 'dayjs';
+
+const AppDatePicker = ({ dateYYYYMMDD, updateDate, label, helperText, error, sx }) => {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        disableFuture
+        openTo='day'
+        views={['year', 'month', 'day']}
+        value={dayjs(dateYYYYMMDD)}
+        onChange={(newDate) => { updateDate(newDate); }}
+        inputFormat="DD MMM YYYY"
+        renderInput={(params) => 
+          <TextField 
+            sx={sx} 
+            label={label} 
+            helperText={error === null ? helperText : error} 
+            size="small" {...params} />
+        }
+      />
+    </LocalizationProvider>
+  );
+}
+
+export default AppDatePicker;

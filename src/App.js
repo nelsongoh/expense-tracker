aed5559e0c13f3379a0176d41dc91d71c4926d78
nelsongoh <61051561+nelsongoh@users.noreply.github.com>
2022-10-09 14:03:08 +0800

    refactor: Currently replaced the App to be a passthrough component at the moment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import { Routes, Route } from 'react-router-dom';
-import Constants from './constants';
-import LandingScene from './scenes/Landing';
-
-const App = () => {
+const App = ({ children }) => {
   return (
-    <Routes>
-      <Route path={Constants.PATHS.INDEX} element={<LandingScene />} />
-    </Routes>
+    {children}
   );
 }
 

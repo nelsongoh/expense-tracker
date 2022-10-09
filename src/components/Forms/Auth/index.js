674a0a61d147a482439d55efb43e9442ca69b254
nelsongoh <61051561+nelsongoh@users.noreply.github.com>
2022-10-09 14:00:07 +0800

    refactor: Renamed the RegisterForm to AuthForm instead

--- src/components/Forms/Auth/index.js
+++ src/components/Forms/Auth/index.js
@@ -3,20 +3,29 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import Styles from './styles';
 import Constants from '../../../constants';
+import { useNavigate } from 'react-router-dom';
 
-const RegisterForm = ({ isMobileDisplay }) => {
+const AuthForm = ({ isMobileDisplay }) => {
   const formStyles = Styles(isMobileDisplay);
+  const navigate = useNavigate();
+
   return (
     <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
       <Paper variant='outlined' sx={formStyles.paper}>
         <Grid container direction="column" spacing={3}>
           <Grid xs display="flex" justifyContent="center" alignItems="center">
-            <Typography variant="h4" sx={formStyles.title}>{Constants.CONTENT.FORMS.REGISTER.TITLE}</Typography>
+            <Typography variant="h4" sx={formStyles.title}>
+              {Constants.CONTENT.FORMS.AUTH.TITLE}
+            </Typography>
           </Grid>
           <Grid xs display="flex" justifyContent="center" alignItems="center">
-            <TextField variant="outlined" label={Constants.CONTENT.FORMS.REGISTER.NAME_FIELD} sx={formStyles.textField} />
+            <TextField variant="outlined" label={Constants.CONTENT.FORMS.AUTH.NAME_FIELD} sx={formStyles.textField} />
+          </Grid>
+          <Grid xs display="flex" justifyContent="center" alignItems="center">
+            <Button variant="contained" onClick={() => { navigate(Constants.PATHS.EXPENSE_FORM) }}>Go to Expense Form</Button>
           </Grid>
         </Grid>
       </Paper>
@@ -24,4 +33,4 @@ const RegisterForm = ({ isMobileDisplay }) => {
   );
 }
 
-export default RegisterForm;
+export default AuthForm;

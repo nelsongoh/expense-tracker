25e0d74fc43c7a7a0696763ef68da4071a900f85
nelsongoh <61051561+nelsongoh@users.noreply.github.com>
2022-10-12 00:39:57 +0800

    refactor: Removed the separate routes file and incorporated it into the App.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { useContext } from 'react';
+import AuthContext from './context/auth/context';
+import { RouterProvider, createBrowserRouter, Navigate } from 'react-router-dom';
+import Constants from "./constants";
+import LandingScene from "./scenes/Landing";
+import ExpenseFormScene from "./scenes/ExpenseForm";
+
 const App = ({ children }) => {
+  const authUser = useContext(AuthContext);
+  const appRouter = createBrowserRouter([
+    {
+      path: Constants.PATHS.INDEX,
+      element: authUser === null ? <LandingScene /> : 
+        <Navigate to={Constants.PATHS.EXPENSE_FORM} replace />,
+    },
+    {
+      path: Constants.PATHS.EXPENSE_FORM,
+      element: authUser !== null ? <ExpenseFormScene /> : <Navigate to={Constants.PATHS.INDEX} replace />
+    }
+  ]);
+
   return (
-    {children}
+    <RouterProvider router={appRouter}>
+      {children}
+    </RouterProvider>
   );
 }
 

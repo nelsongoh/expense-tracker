505216e946154fa475a70ff918a09fc62e6cc9ee
nelsongoh <61051561+nelsongoh@users.noreply.github.com>
2022-10-22 01:07:13 +0800

    feat: Updated the Functions index file to serve static file assets

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.use("/", (req, res) => {
-  res.send();
+const staticAssetPath = path.resolve(__dirname + "/../build/");
+
+// To serve static assets (e.g. CSS, JPG files) from the build folder
+app.use(express.static(staticAssetPath));
+
+app.use("/*", (req, res) => {
+  res.sendFile(staticAssetPath + "/index.html");
 });
 
 exports.app = functions.https.onRequest(app);

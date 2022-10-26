ca08393f0d320d33e842126b5549d63aa94c2281
nelsongoh <61051561+nelsongoh@users.noreply.github.com>
2022-10-26 23:35:03 +0800

    feat: Built a wrapper parent Navigation component with a a mobile-specific Nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,36 @@
+import { useContext, useState, useEffect } from "react";
+import { useLocation, Navigate } from "react-router-dom";
+import DisplayContext from "../../context/display/context";
+import MobileNav from "./mobile";
+import Constants from "../../constants";
+
+const Navbar = () => {
+  const { isMobileDisplay } = useContext(DisplayContext);
+  const location = useLocation();
+  const [currNavIdx, setCurrNavIdx] = useState(null);
+
+  useEffect(() => {
+    const currPath = location.pathname;
+    if (currPath === Constants.PATHS.HOME) {
+      setCurrNavIdx(0);
+    } else if (currPath === Constants.PATHS.CONFIG_ONE) {
+      setCurrNavIdx(2);
+    }
+  }, [location.pathname]);
+
+  // If this is exactly the root path, redirect to the Home scene
+  if (location.pathname === Constants.PATHS.INDEX.ROOT) {
+    return (
+      <Navigate to={Constants.PATHS.HOME} replace />
+    );
+  }
+
+  // TODO: Render the desktop version of the navigation
+  return (
+    isMobileDisplay ? (
+      <MobileNav navValue={currNavIdx} /> 
+    ): null
+  );
+}
+
+export default Navbar;
---
+++ src/components/Nav/mobile.js
@@ -0,0 +1,42 @@
+import { Outlet, useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import CreateIcon from '@mui/icons-material/Create';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Styles from './styles';
+import Constants from '../../constants';
+
+const MobileNav = ({ navValue }) => {
+  const styles = Styles();
+  const navigate = useNavigate();
+
+  return (
+    <Box>
+      <Outlet />
+      <Paper sx={styles.mobileNavBarBackground} elevation={3}>
+        <BottomNavigation showLabels value={navValue}>
+          <BottomNavigationAction 
+            label={Constants.CONTENT.NAVBAR.CREATE} 
+            icon={<CreateIcon />}
+            onClick={() => { navigate(Constants.PATHS.HOME); }}
+          />
+          <BottomNavigationAction 
+            label={Constants.CONTENT.NAVBAR.DASHBOARD} 
+            icon={<AssessmentIcon />} 
+            onClick={() => {  }}  
+          />
+          <BottomNavigationAction 
+            label={Constants.CONTENT.NAVBAR.CONFIG} 
+            icon={<SettingsIcon />} 
+            onClick={() => { navigate(Constants.PATHS.CONFIG_ONE); }}
+          />
+        </BottomNavigation>
+      </Paper>
+    </Box>
+  )
+}
+
+export default MobileNav;

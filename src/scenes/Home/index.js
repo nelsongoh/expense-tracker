dfd952812b438cefbde2be049e01ddc930b5e186
nelsongoh <61051561+nelsongoh@users.noreply.github.com>
2022-10-27 16:09:04 +0800

    refactor: Utilising the React Router's Outlet context to read props for the loaded Outlet insteadnof component props

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,12 +1,24 @@
 import { useContext } from "react";
+import { useLoaderData, useLocation, Outlet } from "react-router-dom";
 import DisplayContext from '../../context/display/context';
-import HomeScreenMobile from "./mobile";
-import HomeScreenDesktop from "./desktop";
+import HomeScreenMobile from "../../components/Home/mobile";
+import HomeScreenDesktop from "../../components/Home/desktop";
+import Constants from "../../constants";
 
 const HomeScene = () => {
   const { isMobileDisplay } = useContext(DisplayContext);
+  const userAppConfig = useLoaderData();
+  const { pathname } = useLocation();
+
   return (
-    isMobileDisplay ? <HomeScreenMobile /> : <HomeScreenDesktop />
+    pathname === Constants.PATHS.HOME ? (
+      isMobileDisplay ? (
+        <HomeScreenMobile existingPaymentMethods={userAppConfig.paymentMethods} />
+       ) : 
+      ( <HomeScreenDesktop /> )
+    ) : (
+      <Outlet context={{ isMobileDisplay: isMobileDisplay }} />
+    )
   );
 }
 

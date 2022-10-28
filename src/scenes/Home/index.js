624c7ad73995a2521c951d2da0aada884fbfadc4
nelsongoh <61051561+nelsongoh@users.noreply.github.com>
2022-10-28 10:51:02 +0800

    refactor: Added in spendCategories as a context prop

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { useLoaderData, useLocation, Outlet } from "react-router-dom";
 import DisplayContext from '../../context/display/context';
 import HomeScreenMobile from "../../components/Home/mobile";
@@ -9,15 +9,26 @@ const HomeScene = () => {
   const { isMobileDisplay } = useContext(DisplayContext);
   const userAppConfig = useLoaderData();
   const { pathname } = useLocation();
+  const [formState, setFormState] = useState({
+    expense: null,
+    income: null,
+  });
 
   return (
     pathname === Constants.PATHS.HOME ? (
       isMobileDisplay ? (
-        <HomeScreenMobile existingPaymentMethods={userAppConfig.paymentMethods} />
+        <HomeScreenMobile 
+          existingPaymentMethods={userAppConfig.paymentMethods} 
+          handleFormState={setFormState}
+        />
        ) : 
       ( <HomeScreenDesktop /> )
     ) : (
-      <Outlet context={{ isMobileDisplay: isMobileDisplay }} />
+      <Outlet context={{ 
+        isMobileDisplay: isMobileDisplay,
+        formState,
+        spendCategories: userAppConfig.spendCategories
+      }} />
     )
   );
 }
